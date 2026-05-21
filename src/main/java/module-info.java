c4797f83d1f763b5a185c66c989e031b50ca19b7
daria-bobko <bobkodaria289@gmail.com>
2026-05-22 01:40:24 +0300

    feat: other files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module university.ua.embroideryproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.swing;
+    requires jdk.compiler;
 
 
     opens university.ua.embroideryproject to javafx.fxml;
